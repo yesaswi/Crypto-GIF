8300ad6c0d409dd1d39bf79a67d0987230ef5769
Yesaswi Vadapalli <yesaswi.vadapalli@gmail.com>
2021-03-17 01:54:34 +0530

    Added code for Android

--- Client/App.js
+++ Client/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import { View, Image, Button, Text, TextInput } from 'react-native'
-import * as ImagePicker from 'react-native-image-picker';
+import { View, Image, Button, Text, TextInput, PermissionsAndroid } from 'react-native'
+// import * as ImagePicker from 'react-native-image-picker';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import CameraRoll from '@react-native-community/cameraroll';
 import RNFetchBlob from 'rn-fetch-blob';
-
+import moment from 'moment';
 
 const createFormData = (photo, body) => {
   const data = new FormData();
@@ -23,29 +24,56 @@ const createFormData = (photo, body) => {
 };
 
 
+const CheckFilePermissions = async (platform) => {
+  if(platform === 'android') {
+    try {
+      const granted = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+      ]);
+      if (granted['android.permission.READ_EXTERNAL_STORAGE'] && granted['android.permission.WRITE_EXTERNAL_STORAGE']) {
+        // user granted permissions
+        return true;
+      } else {
+        // user didn't grant permission... handle with toastr, popup, something...
+        return false;
+      }
+    } catch (err) {
+      // unexpected error
+      return false;
+    }
+  } else {
+    // platform is iOS
+    return true;
+  }
+};
+
+
 export default class App extends React.Component {
 // const [message, onChangeMessage] = React.useState(null);
 // const [secret, onChangeSecret] = React.useState(null);
   state = {
     photo: null,
     message: null,
-    secret: null
+    secret: null,
+    currentDate: null
   }
-
 
   handleChoosePhoto = () => {
     const options = {
-      noData: true,
+      includeBase64: true,
+      saveToPhotos: true
     }
-    ImagePicker.launchImageLibrary(options, response => {
+    launchImageLibrary(options, response => {
       if (response.uri) {
         this.setState({ photo: response })
       }
     })
   }
   
-  handleDecrypt = () => {
-    fetch("http://localhost:5000/decryptsuccess", {
+  handleDecrypt  = async () => {
+    console.log("Uploading to server.....");
+    await fetch("http://192.168.0.123:5000/decryptsuccess", {
       method: "POST",
       headers: {
         'Content-Type': 'multipart/form-data',
@@ -66,11 +94,12 @@ export default class App extends React.Component {
 
   handleEncrypt = async () => {
     console.log("Uploading to server.....");
-    await fetch("http://localhost:5000/success", {
+    await fetch("http://192.168.0.123:5000/success", {
       method: "POST",
       headers: {
         'Content-Type': 'multipart/form-data',
       },
+
       body: createFormData(this.state.photo, { secret: this.state.secret, message: this.state.message })
     })
       .then(response => response.json())
@@ -85,21 +114,54 @@ export default class App extends React.Component {
       });
 
     console.log("Downloading...");
+
+    // if (Platform.OS === 'android') {
+    //   const granted = await this.getPermissionAndroid();
+    //   if (!granted) {
+    //     return;
+    //   }
+    // }
+    if(await CheckFilePermissions(Platform.OS)) {
+      console.log("permission granted", Platform.OS);
+      var date = moment().utcOffset('+05:30');
+      console.log(date);
+      console.log(typeof(date));
+      this.setState({ currentDate: date });
+      console.log("Time", this.state.currentDate);
     await RNFetchBlob.config({
       fileCache: false,
       appendExt: 'png',
+      // path: Platform.OS === 'ios' ? this.state.photo.uri.replace('file://', '') : this.state.photo.uri,
+      path: Platform.OS === "android" ?  RNFetchBlob.fs.dirs.PictureDir + "/" + this.state.currentDate +".png" : photo.uri.replace("file://", "")
+      // addAndroidDownloads: {
+      //   title: "encrypted.png", 
+      //   description: `Download Encrypted`,
+      //   useDownloadManager: false, 
+      //   notification: false,
+      // }
     })
-      .fetch('GET', "http://localhost:5000/encrypt.png")
+      .fetch('GET', "http://192.168.0.123:5000/encrypt.png")
       .then(res => {
-        CameraRoll.save(res.data, 'photo')
+        // console.log(res.data);
+        if((Platform.OS === "android")){
+          // RNFetchBlob.fs.writeFile(RNFetchBlob.fs.dirs.DownloadDir + "/encrypted.png", res.data, 'base64')
+          CameraRoll.save(res.data, {type: 'photo'})
           .then(res => console.log(res))
           .catch(err => console.log(err))
+        }
+        // console.log("LOLOLO",RNFetchBlob.wrap(decodeURIComponent(res.data)))
+        else{
+          CameraRoll.save(res.data)
+          .then(res => console.log(res))
+          .catch(err => console.log(err))
+        }
       })
       .catch(error => console.log(error));
-  };
+  }
+};
 
   clearInput = () => {
-    this.setState({ photo: null, message: null, secret: null});
+    this.setState({ photo: null, message: null, secret: null, currentDate: null});
   }
 
   render() {
